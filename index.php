463a1ef91362bbfa855c72945176901dfd2852a4
shukmuhamad <shukmuhamad@gmail.com>
2022-05-30 14:45:03 +0800

    first project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="0;url=dist/index.php">
+    <title>PQC F34PM</title>
+    <script language="javascript">
+        window.location.href = "F39DD_QREX/login.php"
+    </script>
+</head>
+
+<body>
+    Go to <a href="examples/login.php">/login.php</a>
+</body>
+
+</html>
